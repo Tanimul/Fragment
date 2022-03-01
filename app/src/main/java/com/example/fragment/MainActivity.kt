b809fb274ee12bbf27ac9b7f538fb6b59bfccab3
fuadh <tanimulislam.fuad@gmail.com>
2022-03-01 14:59:40 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.fragment
+
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fragment.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(binding.fragCont.id, HomeFragment())
+            .commit()
+
+        binding.buttomNavigation.setOnItemSelectedListener {
+            Toast.makeText(this, "Clicked : $it", Toast.LENGTH_SHORT).show()
+
+            when (it.itemId) {
+
+                R.id.home ->
+                    supportFragmentManager.beginTransaction()
+                        .replace(binding.fragCont.id, HomeFragment())
+                        .commit()
+                R.id.phone ->
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(binding.fragCont.id, CallFragment())
+                        .commit()
+                R.id.Notification ->
+                    supportFragmentManager.beginTransaction()
+                        .replace(binding.fragCont.id, NotificationFragment())
+                        .commit()
+
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+}
